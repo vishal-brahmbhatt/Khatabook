60b6037b93d7c5da7c4dd0c22368f73e0445409f
Vishal.milkeyway@gmail.com <39566622+vishal-brahmbhatt@users.noreply.github.com>
2020-11-12 02:24:44 +0530

    Login Page Design

--- gen/com/example/khatabook/R.java
+++ gen/com/example/khatabook/R.java
@@ -24,11 +24,18 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int icon=0x7f020001;
+        public static final int rounded_edge=0x7f020002;
+        public static final int rounded_edge_btn=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int imageView1=0x7f080000;
-        public static final int textView1=0x7f080001;
+        public static final int action_settings=0x7f080007;
+        public static final int btn_login=0x7f080003;
+        public static final int et_password=0x7f080002;
+        public static final int et_username=0x7f080001;
+        public static final int imageView1=0x7f080004;
+        public static final int linerLayout1=0x7f080000;
+        public static final int textView1=0x7f080006;
+        public static final int tv_msg1=0x7f080005;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
