c1f9221f4b5495eb2824e41328d5a5166a79c47c
Vishal.milkeyway@gmail.com <39566622+vishal-brahmbhatt@users.noreply.github.com>
2020-11-13 23:29:44 +0530

    Registration design

--- gen/com/example/khatabook/R.java
+++ gen/com/example/khatabook/R.java
@@ -28,15 +28,20 @@ public final class R {
         public static final int rounded_edge_btn=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080008;
+        public static final int action_settings=0x7f08000d;
         public static final int btn_login=0x7f080003;
+        public static final int et_conf_password=0x7f08000a;
+        public static final int et_emailid=0x7f080008;
+        public static final int et_name=0x7f080009;
         public static final int et_password=0x7f080002;
         public static final int et_username=0x7f080001;
         public static final int imageView1=0x7f080005;
         public static final int linerLayout1=0x7f080000;
         public static final int textView1=0x7f080006;
+        public static final int tv_goto_login=0x7f08000c;
         public static final int tv_goto_reg=0x7f080007;
         public static final int tv_msg1=0x7f080004;
+        public static final int tv_passwordnotmatch=0x7f08000b;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
--- src/com/example/khatabook/Registration.java
+++ src/com/example/khatabook/Registration.java
@@ -1,16 +1,66 @@
 package com.example.khatabook;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.opengl.Visibility;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Registration extends Activity {
 
+	TextView  tv_gotologin,tv_matchpassword;
+	EditText  et_password, et_conf_pass;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_registration);
+		tv_gotologin = (TextView)findViewById(R.id.tv_goto_login);
+		tv_matchpassword = (TextView)findViewById(R.id.tv_passwordnotmatch);
+		et_password = (EditText)findViewById(R.id.et_password);
+		et_conf_pass = (EditText)findViewById(R.id.et_conf_password);
+		
+		
+		
+		
+		//////////////////////////////////  tv_gotologin ///////////////////////////////////////
+		
+		tv_gotologin.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent loginIntent = new Intent(Registration.this,Login.class);
+				Registration.this.startActivity(loginIntent);
+				Registration.this.finish();
+			}
+		});
+		
+		////////////////////////////////// et_conf_pass //////////////////////
+
+		et_conf_pass.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+			
+			@Override
+			public void onFocusChange(View v, boolean hasFocus) {
+				// TODO Auto-generated method stub
+				if(hasFocus == false)
+				{
+					if(!et_password.getText().toString().equals(et_conf_pass.getText().toString()))
+					{
+						tv_matchpassword.setVisibility(View.VISIBLE);
+					}
+					else
+					{
+						tv_matchpassword.setVisibility(View.INVISIBLE);
+					}
+					
+				}
+			}
+		});
 	}
 
 	@Override
