6a399ae5d7ca7a714bbcf548999ebf4db4f0c87d
Vishal.milkeyway@gmail.com <39566622+vishal-brahmbhatt@users.noreply.github.com>
2020-11-18 10:25:02 +0530

    API Call

--- src/com/example/khatabook/Registration.java
+++ src/com/example/khatabook/Registration.java
@@ -1,19 +1,33 @@
 package com.example.khatabook;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.content.Intent;
-import android.opengl.Visibility;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Registration extends Activity {
 
 	TextView  tv_gotologin,tv_matchpassword;
-	EditText  et_password, et_conf_pass;
+	EditText  et_password, et_conf_pass,et_email,et_name,et_shopname;
+	Button btn_reg;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +37,12 @@ public class Registration extends Activity {
 		tv_matchpassword = (TextView)findViewById(R.id.tv_passwordnotmatch);
 		et_password = (EditText)findViewById(R.id.et_password);
 		et_conf_pass = (EditText)findViewById(R.id.et_conf_password);
+		et_email = (EditText)findViewById(R.id.et_emailid);
+		et_name = (EditText) findViewById(R.id.et_name);
+		et_shopname = (EditText)findViewById(R.id.et_shopname);
 		
+		
+		btn_reg = (Button)findViewById(R.id.btn_Registration);
 		
 		
 		
@@ -61,6 +80,26 @@ public class Registration extends Activity {
 				}
 			}
 		});
+		
+		
+		///////////////////////////////////// btn_reg on click ///////////////////////////////////////
+		
+		btn_reg.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				String email = et_email.getText().toString();
+				String name = et_name.getText().toString();
+				String shopname = et_shopname.getText().toString();
+				String  password = et_conf_pass.getText().toString();
+				if(Utils.Validate_Form_Registration(email,name,shopname))
+				{
+					new RegistrationTask().execute(shopname,name,email,password);					
+				}
+						
+			}
+		});
 	}
 
 	@Override
@@ -81,4 +120,91 @@ public class Registration extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	
+	////////////////////// Our Custom Class for API Call ////////////
+	private class RegistrationTask extends AsyncTask<String, String, String>{
+	      String result;
+	      
+	      @Override
+	      protected String doInBackground(String... vb) {
+	    	  JSONObject data = new JSONObject();
+				try
+				{
+					URL url = new URL (Utils.get_BaseUrl() + "/usermaster-ins.php");
+					HttpURLConnection con = (HttpURLConnection)url.openConnection();
+					con.setRequestMethod("POST");
+					con.setRequestProperty("Content-Type", "application/json; utf-8");
+					con.setRequestProperty("Accept", "application/json");
+					con.setDoOutput(true);
+					
+					data.put("shopname",vb[0]);
+					data.put("name",vb[1]);
+					data.put("emailid",vb[2]);
+					data.put("address"," ");
+					data.put("gstno"," ");
+					data.put("password", vb[3]);
+					
+					String jsonInputString = data.toString();
+					System.out.println(jsonInputString);
+					
+					OutputStream os = con.getOutputStream();						
+				    byte[] input = jsonInputString.getBytes("utf-8");
+				    os.write(input, 0, input.length);			
+					
+				    BufferedReader br = new BufferedReader(
+	    		    new InputStreamReader(con.getInputStream(), "utf-8"));
+	    
+	    		    StringBuilder response = new StringBuilder();
+	    		    String responseLine = null;
+	    		    while ((responseLine = br.readLine()) != null) {
+	    		        response.append(responseLine.trim());
+	    		    }
+	    		    System.out.println(response.toString());
+	    		    result = response.toString();
+	    		    return response.toString();
+	    		    
+					
+
+				} 
+				catch (MalformedURLException e)
+				{
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} 
+				catch (Exception e) 
+				{
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+	         return null;
+	      }
+	      @Override
+	      protected void onPostExecute(String aVoid) {
+	         
+	         super.onPostExecute(aVoid);
+	         try
+	         {
+				JSONObject res = new JSONObject(result);
+				if(res.get("status").toString().equals("Success"))
+				{
+					Intent loginIntent = new Intent(Registration.this,Login.class);
+					Registration.this.startActivity(loginIntent);
+					Registration.this.finish();
+				}
+				else
+				{
+					Toast.makeText(getApplicationContext(), "Unable to perform registration", Toast.LENGTH_LONG).show();
+				}
+			} 
+	         catch (JSONException e)
+	         {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	         
+	         	
+	      }
+	   }
 }
---
+++ src/com/example/khatabook/Utils.java
@@ -0,0 +1,27 @@
+package com.example.khatabook;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Utils 
+{
+	private static String base_url = "http://192.168.0.54:8080/khatabook/endpoints";
+	
+	public static String get_BaseUrl()
+	{
+		return base_url;
+	}
+	
+	public static boolean isEmailValid(String email) {
+	    String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+	    Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+	    Matcher matcher = pattern.matcher(email);
+	    return matcher.matches();
+	}
+	
+	public static Boolean Validate_Form_Registration(String email,String name,String shopname)
+	{
+		return name!=null && shopname != null && isEmailValid(email);
+	}
+}
+
